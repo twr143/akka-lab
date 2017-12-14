fa8566dde5db7f57caa07939ca9bf4964b8b2483
Ilya Volynin <ilya.volynin@raiffeisen.ru>
2017-12-14 16:23:45 +0300

    file io, 2

--- src/main/scala/streaming/readFile.scala
+++ src/main/scala/streaming/readFile.scala
@@ -1,7 +1,7 @@
 package streaming
 
 import java.io.File
-import java.nio.file.Paths
+import java.nio.file.{Files, Paths}
 
 import akka.Done
 import akka.actor.ActorSystem
@@ -16,29 +16,36 @@ import scala.util.control.NonFatal
 /*
 created by Ilya Volynin at 14.12.17
 */
-object readFile extends App {
+object readFile{
+
   implicit val system = ActorSystem("system")
   implicit val materializer = ActorMaterializer()
-
   def extractId(s: String) = {
     val a = s.split(",")
     a(0) -> a(1)
   }
-
-  private val lineByLineSource = FileIO.fromPath(Paths.get("tmp/example1.csv"))
-    .via(Framing.delimiter(ByteString("\n"), maximumFrameLength = 1024))
-    .map(_.utf8String).recover {
-      case NonFatal(e) => "file not found"
+  def main(args: Array[String]) {
+    val p = Paths.get("tmp/example.csv")
+    if (Files.notExists(p)) {
+      println("file not found")
+      system.terminate()
+      return
     }
-  val future: Future[Done] = lineByLineSource//.filter(_.nonEmpty).filter(_.contains(","))
-    .map(extractId)
-    .scan((false, "", ""))((l, r) => (l._2 != r._1, r._1, r._2))
-    .drop(1)
-    .splitWhen(_._1)
-    .fold(("", Seq[String]()))((l, r) => (r._2, l._2 ++ Seq(r._3)))
-    .concatSubstreams
-    .runForeach(println)
-  private val reply = Await.result(future, 10 seconds)
-  println(s"Received $reply")
-  Await.ready(system.terminate(), 10 seconds)
+
+    val lineByLineSource = FileIO.fromPath(p)
+      .via(Framing.delimiter(ByteString("\n"), maximumFrameLength = 1024))
+      .map(_.utf8String)
+    val future: Future[Done] = lineByLineSource.filter(_.nonEmpty).filter(_.contains(","))
+      .map(extractId)
+      .scan((false, "", ""))((l, r) => (l._2 != r._1, r._1, r._2))
+      .drop(1)
+      .splitWhen(_._1)
+      .fold(("", Seq[String]()))((l, r) => (r._2, l._2 ++ Seq(r._3)))
+      .concatSubstreams
+      .runForeach(println)
+    val reply = Await.result(future, 10 seconds)
+    println(s"Received $reply")
+    Await.ready(system.terminate(), 10 seconds)
+  }
+
 }
