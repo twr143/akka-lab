78b0d9a9e315cf3b763e4eda98ade008837e0ded
twr143 <twr143@gmail.com>
2017-12-17 18:18:26 +0300

    readfiles ungrouped, 1

---
+++ src/main/scala/streaming/readFileGrouping.scala
@@ -0,0 +1,50 @@
+package streaming
+
+import java.io.File
+import java.nio.file.{Files, Paths}
+
+import akka.Done
+import akka.actor.ActorSystem
+import akka.stream.ActorMaterializer
+import akka.stream.scaladsl.{FileIO, Framing}
+import akka.util.ByteString
+
+import scala.concurrent.duration._
+import scala.concurrent.{Await, Future}
+import scala.util.control.NonFatal
+
+/*
+created by Ilya Volynin at 14.12.17
+*/
+object readFileGrouping {
+  implicit val system = ActorSystem("system")
+  implicit val materializer = ActorMaterializer()
+
+  def extractId(s: String): (String, String) = {
+    val a = s.split(",")
+    (a(0), a(1))
+  }
+
+  def main(args: Array[String]) {
+    val p = Paths.get("tmp/ungrouped.csv")
+    if (Files.notExists(p)) {
+      println("file not found")
+      system.terminate()
+      return
+    }
+    val lineByLineSource = FileIO.fromPath(p)
+      .via(Framing.delimiter(ByteString("\r\n"), maximumFrameLength = 1024))
+      .map(_.utf8String)
+    val future: Future[Done] = lineByLineSource.filter(_.nonEmpty).filter(_.contains(","))
+      .map(extractId).fold(Map.empty[String,Int])((l :Map[String,Int], r : (String,String)) =>
+            {
+//              println(l)
+//              println(r)
+              l + (r._1 -> (1 + l.getOrElse(r._1,0)))
+            }
+            ).runForeach(println)
+    val reply = Await.result(future, 10 seconds)
+    println(s"Received $reply")
+    Await.ready(system.terminate(), 10 seconds)
+  }
+}
