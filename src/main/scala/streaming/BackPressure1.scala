43f6aa07a1c80a14fe4b7a8edd8a921f1867ab9e
twr143 <twr143@gmail.com>
2018-01-07 16:20:52 +0300

    backpressure 2

--- src/main/scala/streaming/BackPressure1.scala
+++ src/main/scala/streaming/BackPressure1.scala
@@ -1,42 +1,48 @@
 package streaming
+import akka.NotUsed
 import akka.actor.{Actor, ActorSystem, Props, Scheduler}
 import akka.dispatch.forkjoin.ThreadLocalRandom
 import akka.pattern.Patterns.after
 import akka.stream.scaladsl.{Flow, Keep, Sink, Source}
 import streaming.Consumer2._
 import akka.pattern.Patterns._
-import akka.stream.ActorMaterializer
+import akka.stream.{ActorMaterializer, KillSwitches}
 import streaming.Consumer.Ack
-
 import scala.concurrent.duration._
 import scala.concurrent.{ExecutionContext, Future}
-import scala.util.{Failure, Success}
+import scala.util.{Failure, Success, Try}
 import scala.util.control.NonFatal
 object BackPressure1 extends App {
   implicit val system = ActorSystem()
   implicit val mat = ActorMaterializer()
   implicit val ec = system.dispatcher
   implicit val scheduler = system.scheduler
-  val consumer = system.actorOf(Consumer.props(ec,scheduler), "total")
-  val consumer2 = system.actorOf(Consumer.props(ec,scheduler), "total2")
-
-  val source = Source.fromIterator { () => Iterator.continually(ThreadLocalRandom.current().nextInt(500000)) }.take(4000).watchTermination()((_, futDone) ⇒
-                futDone.onComplete {
-                  case Success(_) ⇒
-                    println("stream successfully completed")
-                    system.terminate()
-                  case Failure(t) ⇒
-                    println(s"failure happened $t ")
-                    system.terminate()
-                })
+  val consumer = system.actorOf(Consumer.props(ec, scheduler, system), "total")
+  val consumer2 = system.actorOf(Consumer.props(ec, scheduler, system), "total2")
+  val source = Source.fromIterator { () => Iterator.range(0, 10000) }.take(40000).watchTermination()((_, futDone) ⇒
+    futDone.onComplete {
+      case Success(a) ⇒
+        println(s"stream successfully completed $a")
+      //system.terminate()
+      case Failure(t) ⇒
+        println(s"failure happened $t ")
+        system.terminate()
+    })
   val toCons = Flow[Int].map(i => {
-    if (i<490000) Process(i, 0) else Complete(i)
+    if (i < 490000) Process(i, 0) else Complete(i)
   })
   val sinkConsumer = Sink.actorRefWithAck(consumer, Init, Ack, Complete(1000), errorHandler)
   val sinkConsumer2 = Sink.actorRefWithAck(consumer2, Init, Ack, Complete(1000), errorHandler)
-
-        //val stream = source.take(100).via(toCons).toMat(sinkConsumer)(Keep.both).run()
-          toCons/*.alsoTo(sinkConsumer2)*/.to(sinkConsumer).runWith(source)
+  val lastSnk = Sink.last[Any]
+  val ((killSwitch, last), NotUsed) = source.via(toCons).viaMat(KillSwitches.single)(Keep.right).alsoToMat(lastSnk)(Keep.both).toMat(sinkConsumer)(Keep.both).run()
+  //          toCons/*.alsoTo(sinkConsumer2)*/.viaMat(KillSwitches.single)(Keep.right).toMat(sinkConsumer)(Keep.right).runWith(source)
+  Thread.sleep(80)
+  killSwitch.shutdown()
+  last.onComplete {
+    case Success(Process(l,_)) => println(s"the last one is $l")
+    case Success(x) => println(s"completed with unexpected message $x")
+    case Failure(t) => println("An error has occured whil completion of last: " + t.getMessage)
+  }
 }
 object Consumer {
   case object Init
@@ -48,10 +54,9 @@ object Consumer {
       case NonFatal(e) => println("exception happened: " + e)
     }
   }
-  def props(implicit ec: ExecutionContext, scheduler: Scheduler): Props = Props(new Consumer()(ec,scheduler))
+  def props(implicit ec: ExecutionContext, scheduler: Scheduler, system: ActorSystem): Props = Props(new Consumer()(ec, scheduler, system))
 }
-
-class Consumer(implicit ec: ExecutionContext, scheduler: Scheduler) extends Actor {
+class Consumer(implicit ec: ExecutionContext, scheduler: Scheduler, system: ActorSystem) extends Actor {
   override def receive: Receive = {
     case _: Init.type =>
       println(s"init")
@@ -59,38 +64,10 @@ class Consumer(implicit ec: ExecutionContext, scheduler: Scheduler) extends Acto
     case Process(value, _) =>
       println(s"v=$value :${Thread.currentThread().getId}")
       if (value > 450000) sender ! errorHandler(new IllegalStateException("too large value"))
-        sender ! Ack
+      sender ! Ack
     case Complete(id) =>
       println(s"completed $id")
+      system.terminate()
       sender ! Ack
   }
 }
-
-
-/*class Consumer(implicit ec: ExecutionContext, scheduler: Scheduler) extends Actor {
-  override def receive: Receive = {
-    case _: Init.type =>
-      pipe(init(), ec).to(sender())
-    case Process(value, m) =>
-      pipe(process(value, m), ec).to(sender())
-    case Complete(id) =>
-      pipe(complete(id), ec).to(sender())
-  }
-
-  def init(): Future[Consumer.Ack] = {
-    println(s"init получено")
-    Future(Ack())(ec)
-//    after(30.millis, scheduler, ec, Future(Ack())(ec))
-  }
-
-  def process(value: Long, lastMessage: Long): Future[Any] = {
-    println(s"v=$value")
-    after(30.millis, scheduler, ec, Future(if (value > 450000)
-      errorHandler(new IllegalStateException("too large value")) else Ack())(ec))
-  }
-  def complete(value: Long): Future[Ack] = {
-    println(s"completed $value")
-    after(30.millis, scheduler, ec, Future(Ack())(ec))
-  }
-
-} */
