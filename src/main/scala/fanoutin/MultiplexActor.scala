668a362f32a0ecdb3717af0019670deb452486e5
twr143 <twr143@gmail.com>
2018-06-10 11:31:13 +0300

    multiplexing-demultiplexing, after, 2

--- src/main/scala/fanoutin/MultiplexActor.scala
+++ src/main/scala/fanoutin/MultiplexActor.scala
@@ -1,27 +1,32 @@
 package fanoutin
-import akka.actor.{Actor, ActorLogging, ActorRef, Props}
+import akka.actor.{Actor, ActorLogging, ActorRef, ActorSystem, Props}
+import akka.pattern.Patterns.after
 import fanoutin.MultiplexActor._
+
 import scala.concurrent.{ExecutionContext, Future}
 import akka.pattern._
+
+import scala.concurrent.duration._
 /**
   * Created by Ilya Volynin on 10.06.2018 at 8:35.
   */
 class MultiplexActor extends Actor with ActorLogging {
   implicit val ec: ExecutionContext = context.dispatcher
+  implicit val system = ActorSystem()
 
   override def receive: Receive = {
     case c: CommandToMultiplex =>
-      log.debug("incoming {}", c.data)
+      log.info("incoming {}", c.data)
       // no real reason to fan in into the same actor
       // since actor queue will be shared among incoming and outgoing messages
       pipe(parallelCalc(sender(), c)).to(self)
     case c: CommandToDeMultiplex =>
-      log.debug("outgoing {}", c.processedData)
+      log.info("outgoing {}", c.processedData)
       c.sender ! c.processedData
   }
 
   private def parallelCalc(sender: ActorRef, cmd: CommandToMultiplex): Future[CommandToDeMultiplex] = {
-    Future.successful(CommandToDeMultiplex(sender, ProcessedData(cmd.data.counter * (-1))))
+    after(900.millis, system.scheduler, ec, Future.successful(CommandToDeMultiplex(sender, ProcessedData(cmd.data.counter * (-1)))))
   }
 }
 object MultiplexActor {
