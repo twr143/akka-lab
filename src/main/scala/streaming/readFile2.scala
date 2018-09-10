8ec3530b3d73f39b653a02d3711565b3b2332cac
twr143 <twr143@gmail.com>
2018-09-10 09:22:38 +0300

    counting number of lines of source files, 1

--- src/main/scala/streaming/readFile2.scala
+++ src/main/scala/streaming/readFile2.scala
@@ -7,6 +7,10 @@ import akka.stream.ActorMaterializer
 import akka.stream.scaladsl.{FileIO, Flow, Framing, Keep, Sink, Source}
 import akka.util.ByteString
 import streaming.BackPressure1.system
+import scala.concurrent.duration._
+
+
+import scala.concurrent.Await
 /**
   * Created by Ilya Volynin on 16.06.2018 at 14:35.
   */
@@ -18,11 +22,15 @@ object readFile2 extends App {
   // need to test
   // Given a stream of bytestrings delimited by the system line separator we can get lines represented as Strings
   val lines = Framing.delimiter(
-    ByteString(System.lineSeparator), 10000, allowTruncation = true).map(bs => bs.utf8String)
+    ByteString("\n"), maximumFrameLength = 1024).map(bs => bs.utf8String)
   // given as stream of Paths we read those files and count the number of lines
-  val lineCounter = Flow[Path].flatMapConcat(path => FileIO.fromPath(path).fold(0l)((count, line) => count + 1)).toMat(Sink.head)(Keep.right)
+  val lineCounter = Flow[String].fold(0l)((count, line) => count + 1).toMat(Sink.head)(Keep.right)
   // Here's our test data source (replace paths with real paths)
-  val testFiles = Source(List("somePathToFile1", "somePathToFile2").map(new File(_).toPath))
+  val testFiles = Source(List("tmp/ungrouped.csv", "tmp/example.csv").map(new File(_).toPath)).flatMapConcat(FileIO.fromPath(_)).via(lines)
   // Runs the line counter over the test files, returns a Future, which contains the number of lines, which we then print out to the console when it completes
-  testFiles runWith lineCounter foreach println
+  val resFuture = testFiles.runWith(lineCounter)
+  val reply = Await.result(resFuture, 10 seconds)
+  println(s"Received $reply")
+  Await.ready(system.terminate(), 10 seconds)
+
 }
