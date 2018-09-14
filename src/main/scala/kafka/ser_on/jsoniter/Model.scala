d5785e8b23aca1019b1b335af3c79a722419cac3
twr143 <twr143@gmail.com>
2018-09-14 15:13:21 +0300

    multitype kafka stream submissions, 2

--- src/main/scala/kafka/ser_on/jsoniter/Model.scala
+++ src/main/scala/kafka/ser_on/jsoniter/Model.scala
@@ -14,6 +14,5 @@ object Model {
   case class SerializationBeanJsoniter(first: String, second: Int, third: java.time.OffsetDateTime) extends SerializationBeanJsoniterBase
   case class SerializationBeanJsoniter2(first: String, second: Int, third: java.time.OffsetDateTime, fourth: Int) extends SerializationBeanJsoniterBase
   implicit val codec: JsonValueCodec[SerializationBeanJsoniterBase] = JsonCodecMaker.make[SerializationBeanJsoniterBase](CodecMakerConfig())
-//  implicit val codec2: JsonValueCodec[SerializationBeanJsoniter2] = JsonCodecMaker.make[SerializationBeanJsoniter2](CodecMakerConfig())
 
 }
