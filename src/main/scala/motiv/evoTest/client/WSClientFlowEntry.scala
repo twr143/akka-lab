245e87c25f98d1036e5e896b0c4e18eaac1653e3
twr143 <twr143@gmail.com>
2018-11-15 12:09:50 +0300

    WSClient test extension, 1

--- src/main/scala/motiv/evoTest/client/WSClientFlowEntry.scala
+++ src/main/scala/motiv/evoTest/client/WSClientFlowEntry.scala
@@ -17,24 +17,29 @@ import scala.concurrent.duration._
 import motiv.evoTest.Util._
 
 // sample run: "runMain motiv.evoTest.client.WSClientFlowEntry 21 40"
-
 object WSClientFlowEntry extends StreamWrapperApp {
 
   override def body(args: Array[String])(implicit as: ActorSystem, mat: ActorMaterializer, ec: ExecutionContext): Future[Any] = {
     if (args.length != 2) return Future.failed(new Exception("please provide first and last indexes of range for adding table as program arguments. Thanks."))
-//
-    val incoming: Sink[Message, Future[Done]] =
-    Sink.foreach {
-      case message: TextMessage.Strict =>
-        val out = readFromArray[Outgoing](message.text.getBytes("UTF-8"))
-        println(out)
-    }
     //
+    val incoming: Sink[Message, Future[Done]] =
+      Sink.foreach {
+        case message: TextMessage.Strict =>
+          val out = readFromArray[Outgoing](message.text.getBytes("UTF-8"))
+          println(out)
+      }
+    //
+    val (start, end) = (args(0).toInt, args(1).toInt)
     val loginSource = Source.single(Login("admin", "admin"))
-    val iter = Iterator.range(args(0).toInt, args(1).toInt)
-    val newTables = Source.fromIterator(() => iter).map(i => AddTable(Table(i + 2, "table - Foo Fighters " + i * i, 4 * i), i + 1))
+    val iterFirstHalf = Iterator.range(start, start + (end - start) / 2)
+    val iterSecondHalf = Iterator.range(start + (end - start) / 2 + 1, end)
+    val newTablesFirstHalf = Source.fromIterator(() => iterFirstHalf).map(i => AddTable(Table(i + 2, "table - Foo Fighters " + i * i, 4 * i), i + 1))
+    val newTablesSecondHalf = Source.fromIterator(() => iterSecondHalf).map(i => AddTable(Table(i + 2, "table - Foo Fighters " + i * i, 4 * i), i + 1))
     val subscribeSource = Source.single(SubscribeTables)
-    val aggSource = Source.combine[Incoming, Incoming](loginSource, subscribeSource, newTables)(Concat(_)).map(i => TextMessage(writeToArray[Incoming](i))
+    val unSubscribeSource = Source.single(UnsubscribeTables)
+    val aggSource = Source.combine[Incoming, Incoming](loginSource,
+      subscribeSource, newTablesFirstHalf, 
+      unSubscribeSource, newTablesSecondHalf)(Concat(_)).map(i => TextMessage(writeToArray[Incoming](i))
     )
     //
     val webSocketFlow = Http().webSocketClientFlow(WebSocketRequest("ws://localhost:9000/ws_api"))
@@ -52,7 +57,6 @@ object WSClientFlowEntry extends StreamWrapperApp {
       .viaMat(webSocketFlow)(Keep.right) // keep the materialized Future[WebSocketUpgradeResponse]
       .toMat(incoming)(Keep.both) // also keep the Future[Done]
       .run()
-
     // just like a regular http request we can access response status which is available via upgrade.response.status
     // status code 101 (Switching Protocols) indicates that server support WebSockets
     val connected = upgradeResponse.flatMap { upgrade =>
