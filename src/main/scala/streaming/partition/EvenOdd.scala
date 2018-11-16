7e513060003b53d608e3f72742d57f8bb91caa59
twr143 <twr143@gmail.com>
2018-11-16 19:06:10 +0300

    even-odd, refactoring

--- src/main/scala/streaming/partition/EvenOdd.scala
+++ src/main/scala/streaming/partition/EvenOdd.scala
@@ -2,25 +2,27 @@ package streaming.partition
 import akka.actor.ActorSystem
 import akka.stream.{ActorMaterializer, ClosedShape}
 import akka.stream.scaladsl.{GraphDSL, Partition, RunnableGraph, Sink, Source}
+import util.StreamWrapperApp
+import scala.concurrent.{ExecutionContext, Future}
 import scala.io.StdIn
+
 /**
   * Created by Ilya Volynin on 15.06.2018 at 16:45.
   */
-object EvenOdd extends App {
-  implicit val system = ActorSystem()
-  implicit val mat = ActorMaterializer()
-  val numbers = Source[Int](List(1, 2, 3, 4, 5, 6))
-  val oddNumbers = Sink.foreach[Int](number => println(s"odd \t$number"))
-  val evenNumbers = Sink.foreach[Int](number => println(s"even \t$number"))
+object EvenOdd extends StreamWrapperApp {
 
-  RunnableGraph.fromGraph(GraphDSL.create() { implicit b =>
-    import GraphDSL.Implicits._
-    val partition = b.add(Partition[Int](2, number => if (number % 2 == 0) 1 else 0))
-    numbers ~> partition.in
-    partition.out(0) ~> oddNumbers
-    partition.out(1) ~> evenNumbers
-    ClosedShape
-  }).run()
-  Thread.sleep(100)
-  system.terminate()
+  def body(args: Array[String])(implicit as: ActorSystem, mat: ActorMaterializer, ec: ExecutionContext): Future[Any] = {
+    val numbers = Source[Int](List(1, 2, 3, 4, 5, 6))
+    val oddNumbers = Sink.foreach[Int](number => println(s"odd \t$number"))
+    val evenNumbers = Sink.foreach[Int](number => println(s"even \t$number"))
+    RunnableGraph.fromGraph(GraphDSL.create(oddNumbers, evenNumbers)((a, _) => a) { implicit b =>
+      import GraphDSL.Implicits._
+      (oddNumbers, evenNumbers) =>
+        val partition = b.add(Partition[Int](2, number => (number + 1) % 2))
+        numbers ~> partition.in
+        partition.out(0) ~> oddNumbers
+        partition.out(1) ~> evenNumbers
+        ClosedShape
+    }).run()
+  }
 }
--- src/main/scala/streaming/partition/EvenOddResults.scala
+++ src/main/scala/streaming/partition/EvenOddResults.scala
@@ -1,39 +1,24 @@
 package streaming.partition
+import akka.NotUsed
 import akka.actor.ActorSystem
 import akka.stream.{ActorMaterializer, ClosedShape}
-import akka.stream.scaladsl.{Flow, GraphDSL, Partition, RunnableGraph, Sink, Source}
+import akka.stream.scaladsl.{Flow, GraphDSL, Keep, Partition, RunnableGraph, Sink, Source}
+import util.StreamWrapperApp
 import scala.collection.mutable._
+import scala.concurrent.{ExecutionContext, Future}
+
 /**
   * Created by Ilya Volynin on 15.06.2018 at 17:05.
   */
-object EvenOddResults extends App {
-  implicit val system = ActorSystem()
-  implicit val mat = ActorMaterializer()
-  //global variables - not good
-  val errorMap: Map[Int, ListBuffer[Int]] = Map(1 -> ListBuffer[Int]())
-  val validMap: Map[Int, ListBuffer[Int]] = Map(1 -> ListBuffer[Int]())
-  val numbers = Source[Int](List(1, 2, 3, 4, 5, 6))
-  val errCheck = Flow[Int].map(i => {
-    if (i % 2 == 0) errorMap.get(1).map(_.+=(i)) else validMap.get(1).map(_.+=(i))
-    i
-  })
-  val afterStreamProcess = Flow[Int].map(i => {
-    if (errorMap.get(1).map(_.size).getOrElse(0) +
-      validMap.get(1).map(_.size).getOrElse(0) >= 6 /*assume we know total size*/ ) {
-      alertSourceCompleted(errorMap.getOrElse(1, ListBuffer[Int]()), validMap.getOrElse(1, ListBuffer[Int]()))
-      errorMap -= 1
-      validMap -= 1
-    }
-    i
-  })
-  RunnableGraph.fromGraph(GraphDSL.create() { implicit b =>
-    import GraphDSL.Implicits._
-    numbers ~> errCheck ~> afterStreamProcess ~> Sink.ignore
-    ClosedShape
-  }).run()
-  //numbers.via(errCheck).to(Sink.ignore).run()
-  Thread.sleep(100)
-  system.terminate()
+object EvenOddResults extends StreamWrapperApp {
+
+  def body(args: Array[String])(implicit as: ActorSystem, mat: ActorMaterializer, ec: ExecutionContext): Future[Any] = {
+    val numbers = Source[Int](List(1, 2, 3, 4, 5, 6))
+    val errCheck = Flow[Int].
+      fold(ListBuffer[Int](), ListBuffer[Int]())((lists, number) =>
+        if (number % 2 == 0) (lists._1 :+ number, lists._2) else (lists._1, lists._2 :+ number))
+    numbers.via(errCheck).runWith(Sink.foreach(s => alertSourceCompleted(s._1, s._2)))
+  }
 
   def alertSourceCompleted(errors: ListBuffer[Int], valids: ListBuffer[Int]): Unit = {
     println(s"errors: $errors")
