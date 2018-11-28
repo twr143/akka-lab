5f9bc3985edaadcd069bd60fea2a19d3b0827b34
twr143 <twr143@gmail.com>
2018-11-28 21:19:48 +0300

    branching pipeline, 1

---
+++ src/main/scala/streaming/partition/Branching.scala
@@ -0,0 +1,46 @@
+package streaming.partition
+import akka.Done
+import akka.actor.ActorSystem
+import akka.stream.{ActorMaterializer, FlowShape}
+import akka.stream.scaladsl.{Flow, GraphDSL, Partition, RunnableGraph, Sink, Source}
+import akka.stream.scaladsl.GraphDSL.Implicits._
+import ch.qos.logback.classic.Logger
+import util.StreamWrapperApp2
+import scala.concurrent.{ExecutionContext, Future}
+
+/**
+  * Created by Ilya Volynin on 28.11.2018 at 20:15.
+  * Many thanks to Stefano Bonetti for his article (and video presentation)
+  * https://speakerdeck.com/svezfaz/stream-driven-development-design-your-data-pipeline-with-akka-streams
+  */
+object Branching extends StreamWrapperApp2 {
+
+  override def body(args: Array[String])
+                   (implicit as: ActorSystem, mat: ActorMaterializer, ec: ExecutionContext, logger: Logger): Future[Any] = {
+    val numbers = Source[Int](List(1, 2, 3, 4, 5, 6))
+    val s1 = Sink.foreach[Int](number => logger.warn("%3=0: \t{}", number))
+    val s2 = Sink.foreach[Int](number => logger.warn("%3=1: \t{}", number))
+    val s3 = Sink.foreach[Int](number => logger.warn("%3=2: \t{}", number))
+
+    numbers.map(err1Func).via(divertErrors[Int, Int, Future[Done]](s1))
+      .map(err2Func).via(divertErrors[Int, Int, Future[Done]](s2)).runWith(s3)
+  }
+
+  def err1Func(n: Int): Either[Int, Int] = if (n % 3 == 0) Left(n) else Right(n)
+  def err2Func(n: Int): Either[Int, Int] = if (n % 3 == 1) Left(n) else Right(n)
+
+  def divertErrors[T, E, M](to: Sink[E, M]): Flow[Either[E, T], T, M] = {
+    Flow.fromGraph(GraphDSL.create(to) {
+      implicit b =>
+        sink =>
+          val partition = b.add(Partition[Either[E, T]](2, _.fold(_ => 0, _ => 1)))
+          val left = b.add(Flow[Either[E, T]].map(_.left.get))
+          val right = b.add(Flow[Either[E, T]].map(_.right.get))
+          partition ~> left ~> sink
+          partition ~> right
+          FlowShape(partition.in, right.out)
+    })
+  }
+}
+
+
