a24ef3a9f070ac536236875cb97213feea449e24
twr143 <twr143@gmail.com>
2018-11-29 19:49:48 +0300

    branching pipeline, 3

--- src/main/scala/streaming/partition/Branching.scala
+++ src/main/scala/streaming/partition/Branching.scala
@@ -2,10 +2,11 @@ package streaming.partition
 import akka.Done
 import akka.actor.ActorSystem
 import akka.stream.{ActorMaterializer, FlowShape}
-import akka.stream.scaladsl.{Flow, GraphDSL, Partition, RunnableGraph, Sink, Source}
+import akka.stream.scaladsl.{Flow, GraphDSL, Keep, Partition, RunnableGraph, Sink, Source}
 import akka.stream.scaladsl.GraphDSL.Implicits._
 import ch.qos.logback.classic.Logger
 import util.StreamWrapperApp2
+
 import scala.concurrent.{ExecutionContext, Future}
 
 /**
@@ -17,17 +18,23 @@ object Branching extends StreamWrapperApp2 {
 
   override def body(args: Array[String])
                    (implicit as: ActorSystem, mat: ActorMaterializer, ec: ExecutionContext, logger: Logger): Future[Any] = {
+
     val numbers = Source[Int](List(1, 2, 3, 4, 5, 6))
     val s1 = Sink.foreach[Int](logger.warn("%3=0: \t{}", _))
     val s2 = Sink.foreach[Int](logger.warn("%3=1: \t{}", _))
     val s3 = Sink.foreach[Int](logger.warn("%3=2: \t{}", _))
-
-    numbers.map(err1Func).via(divertErrors[Int, Int, Future[Done]](s1))
+    val s4 = Sink.foreach[Int](logger.warn("%60==0: \t{}", _))
+    val flow1 = Flow[Int].map(_ * 10).map(errBranchFunc).via(divertErrors(s4))
+    numbers.map(err1Func).via(divertErrors[Int, Int, Future[Done]](flow1.toMat(s1)(Keep.right)))
       .map(err2Func).via(divertErrors[Int, Int, Future[Done]](s2)).runWith(s3)
+
   }
 
   def err1Func(n: Int): Either[Int, Int] = if (n % 3 == 0) Left(n) else Right(n)
+
   def err2Func(n: Int): Either[Int, Int] = if (n % 3 == 1) Left(n) else Right(n)
+
+  def errBranchFunc(n: Int): Either[Int, Int] = if (n % 60 == 0) Left(n) else Right(n)
 
   def divertErrors[T, E, M](to: Sink[E, M]): Flow[Either[E, T], T, M] = {
     Flow.fromGraph(GraphDSL.create(to) {
